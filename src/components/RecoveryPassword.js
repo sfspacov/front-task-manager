72ee514b020fb0d9843b1a387bbddf3420e6f42e
sfspacov <stephan_fs@hotmail.com>
2024-08-02 12:21:06 -0500

    toast recovery

--- src/components/RecoveryPassword.js
+++ src/components/RecoveryPassword.js
@@ -4,83 +4,87 @@ import 'react-toastify/dist/ReactToastify.css'; // Import Toastify CSS
 import '../css/Login.css'; // Import the CSS file
 
 const RecoveryPassword = ({ onClose }) => {
-  const [email, setEmail] = useState('');
+    const [email, setEmail] = useState('');
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await fetch('http://localhost:2000/recover-password', {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email }),
-      });
-      
-      if (response.ok) {
-        toast.success('Password recovery email sent successfully!', {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        });
-        setTimeout(() => {
-            onClose();
-          }, 5000); // 3000 milliseconds = 3 seconds
-      } else {
-        const data = await response.json();
-        toast.error(data.message || 'Failed to send recovery email', {
-          position: "top-center",
-          autoClose: 5000,
-          hideProgressBar: false,
-          closeOnClick: true,
-          pauseOnHover: true,
-          draggable: true,
-          progress: undefined,
-          theme: "colored",
-        });
-      }
-    } catch (error) {
-      console.error('Error:', error);
-      toast.error('An error occurred. Please try again.', {
-        position: "top-center",
-        autoClose: 5000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
-    }
-  };
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const toastId = toast.loading('Please wait, your email is being sent!',{
+                position: "top-center",
+            });
+            const response = await fetch('http://localhost:2000/recover-password', {
+                method: 'PUT',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ email }),
+            });
 
-  return (
-    <div className="login-container">
-      <ToastContainer /> {/* Add ToastContainer for toasts */}
-      <div className="login-box">
-        <h2>Recover Password</h2>
-        <form onSubmit={handleSubmit}>
-          <div className="mb-3">
-            <label htmlFor="email" className="form-label">E-mail</label>
-            <input
-              type="email"
-              className="form-control"
-              value={email}
-              onChange={(e) => setEmail(e.target.value)}
-              required
-            />
-          </div>
-          <button type="submit" className="btn btn-primary">Send</button>
-          <button type="button" className="btn btn-danger" onClick={onClose}>Cancel</button>
-        </form>
-      </div>
-    </div>
-  );
+            toast.dismiss(toastId);
+            if (response.ok) {
+                toast.success('Password recovery email has been sent successfully!', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+                setTimeout(() => {
+                    onClose();
+                }, 5000); // 3000 milliseconds = 3 seconds
+            } else {
+                const data = await response.json();
+                toast.error(data.message || 'Failed to send recovery email', {
+                    position: "top-center",
+                    autoClose: 5000,
+                    hideProgressBar: false,
+                    closeOnClick: true,
+                    pauseOnHover: true,
+                    draggable: true,
+                    progress: undefined,
+                    theme: "colored",
+                });
+            }
+        } catch (error) {
+            console.error('Error:', error);
+            toast.error('An error occurred. Please try again.', {
+                position: "top-center",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+                theme: "colored",
+            });
+        }
+    };
+
+    return (
+        <div className="login-container">
+            <ToastContainer /> {/* Add ToastContainer for toasts */}
+            <div className="login-box">
+                <h2>Recover Password</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="mb-3">
+                        <label htmlFor="email" className="form-label">E-mail</label>
+                        <input
+                            type="email"
+                            className="form-control"
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="btn btn-primary">Send</button>
+                    <button type="button" className="btn btn-danger" onClick={onClose}>Cancel</button>
+                </form>
+            </div>
+        </div>
+    );
 };
 
 export default RecoveryPassword;
